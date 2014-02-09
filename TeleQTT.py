9ad703742726eff0d42feaf2b18d429aa9709c98
Romain BOURDY <romain@bourdy.eu>
2014-02-09 17:34:31 +0100

    first commit

---
+++ TeleQTT.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+import mosquitto
+import os
+import teleinfo
+from serial import SerialException
+
+broker = "localhost"
+port = 1883
+
+def on_connect(mosq, obj, rc):
+    if rc == 0:
+    #rc 0 successful connect
+        print "Connected"
+    else:
+        raise Exception
+ 
+def on_publish(mosq, obj, mid):
+    print("Message "+str(mid)+" published.")
+ 
+ 
+#called on exit
+#close serial, disconnect MQTT
+ 
+def cleanup():
+    print "Ending and cleaning up"
+    mqttc.disconnect()
+
+try:
+    print "Connecting... "
+    #connect to serial port
+    ti = teleinfo.Teleinfo("/dev/teleinfo")
+except:
+    print "Failed to connect serial"
+    #unable to continue with no serial input
+    raise SystemExit
+
+try:
+    mypid = os.getpid()
+    client_uniq = "RB_EDF"
+    mqttc = mosquitto.Mosquitto(client_uniq)
+
+#attach MQTT callbacks
+    mqttc.on_connect = on_connect
+    mqttc.on_publish = on_publish
+
+#connect to broker
+    mqttc.connect(broker, port, 60, True)
+ 
+    while mqttc.loop() == 0:
+        line = ti.read()
+        if line is not None:
+            for serEtiquette, serValue in line.items():
+                mqttc.publish("EDF/"+serEtiquette, serValue)
+except (SerialException):
+    print "Serial Exception"
+#    pass
+except (IndexError):
+    print "No data received within serial timeout period"
+    cleanup()
+# handle app closure
+except (KeyboardInterrupt):
+    print "Interrupt received"
+    cleanup()
+except (RuntimeError):
+    print "uh-oh! time to die"
+    cleanup()
---
+++ teleinfo.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import serial
+
+class Teleinfo:
+
+        ser = serial.Serial()
+        
+        def __init__ (self, port='/dev/teleinfo'):
+                self.ser = serial.Serial(port, baudrate=1200, bytesize=serial.SEVENBITS, parity=serial.PARITY_EVEN)
+        
+        def checksum (self, etiquette, valeur):
+                sum = 32
+                for c in etiquette: sum = sum + ord(c)
+                for c in valeur:        sum = sum + ord(c)
+                sum = (sum & 63) + 32
+                return chr(sum)
+                
+        def read (self):
+                # Wait for data
+            while self.ser.inWaiting() : 
+                message = ""
+                completed = False
+                
+                while not completed:
+                        char = self.ser.read(1)
+                        if char != chr(2):
+                                message = message + char
+                        else:
+                                completed = True
+                
+                frames = [
+                        frame.split(" ")
+                        for frame in message.strip("\r\n\x03").split("\r\n")
+                        ]
+                        
+                framesOK = dict([
+                        [frame[0],frame[1]]
+                        for frame in frames
+                        if (len(frame) == 3) and (self.checksum(frame[0],frame[1]) == frame[2])
+                        ])
+                        
+                return framesOK
